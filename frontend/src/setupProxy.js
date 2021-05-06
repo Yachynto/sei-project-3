5630e0ebedd5ba606572e30d854225e4f9cb12b0
giacinto90 <giacinto.am@gmail.com>
2021-05-06 12:16:41 +0100

    linked to atlas

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,7 +1,7 @@
-const { createProxyMiddleware } = require('http-proxy-middleware')
+// const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
-  app.use(createProxyMiddleware('/api', { target: 'http://fixie:DOTs93lGtrOQlNg@velodrome.usefixie.com:80' }))
+  app.use('/api', { target: 'http://fixie:DOTs93lGtrOQlNg@velodrome.usefixie.com:80' })
 }
 
 // module.exports = function (app) {
